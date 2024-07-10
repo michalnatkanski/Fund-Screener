45343238f61f3504e8fe07bd5775843c2bfd0588
mnatkanski <m.natkanski0@gmail.com>
2024-07-10 10:36:13 +0200

    Add ts to Header component

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,18 +1,16 @@
-import styles from './Header.module.scss';
+import styles from './Header.module.scss'
 
-const Header = () =>
-
+const Header = () => (
     <header className={styles.container}>
         <div className={styles.container__content}>
             <img src={'./logo.png'} alt='logo' />
             <h1 className={styles.container__content_title}>Our funds</h1>
             <p className={styles.container__content_description}>
-                Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit.
-                Donec elementum ultricies nisi,
-                fermentum bibendum lacus fermentum venenatis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum ultricies nisi, fermentum
+                bibendum lacus fermentum venenatis.
             </p>
         </div>
     </header>
+)
 
-export default Header;
+export default Header
