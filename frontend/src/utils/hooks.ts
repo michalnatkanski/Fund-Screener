8b5b89e958da2612064e64daac7fc83cec68bc7b
mnatkanski <m.natkanski0@gmail.com>
2024-07-10 17:49:25 +0200

    Update typescript

--- frontend/src/utils/hooks.ts
+++ frontend/src/utils/hooks.ts
@@ -1,6 +1,6 @@
   import { useState, useEffect } from "react";
   //debounce hook for search
-  export function useSearchDebounce(value, delay) {
+  export function useSearchDebounce(value:string, delay:number) {
     const [search, setSearch] = useState(value);
     useEffect(() => {
       const delayFn = setTimeout(() => setSearch(value), delay);
