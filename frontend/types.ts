2fc542e48da49e5dd7c78403a4706862c04537e1
mnatkanski <m.natkanski0@gmail.com>
2024-07-11 21:16:02 +0200

    Update App types

--- frontend/types.ts
+++ frontend/types.ts
@@ -1,36 +1,35 @@
-
 export type Fund = {
-    currency: string
-    fundName: string
-    name: string
-    region: string
-    isin: string
-    type: string
+    currency: string;
+    fundName: string;
+    name: string;
+    region: string;
+    isin: string;
+    type: string;
     performance: {
-        asOfDate: string
+        asOfDate: string;
         data?: {
-            period: string
-            value: number
-        }[]
-    }
-    read: any
-    launchDate: string
+            period: string;
+            value: number;
+        }[];
+    };
+    read: any;
+    launchDate: string;
     m1: { period: string; value: number; };
     m3: { period: string; value: number; };
     m6: { period: string; value: number; };
-    y1:  { period: string; value: number; };
+    y1: { period: string; value: number; };
     y3: { period: string; value: number; };
-    si:  { period: string; value: number; };
+    si: { period: string; value: number; };
     asOfDate: string;
-}
+};
 
-export type Funds = Fund[]
+export type Funds = Fund[];
 
 export type FilterOptions = {
-    currency: string
-    region: string
-    type: string
-}
+    currency: string;
+    region: string;
+    type: string;
+};
 
 export interface OverviewType {
     fundName: string;
@@ -39,19 +38,21 @@ export interface OverviewType {
 
 export interface PerformanceType {
     fundName: string;
-    fund: PerformanceFund
+    fund: PerformanceFund[];
 }
 
 export interface ReducedFunds {
-    overview: OverviewType[]
-    performance: PerformanceType[]
+    overview: OverviewType[];
+    performance: PerformanceType[];
 }
 
 type PerformanceFund = {
-        name: string;
-        m1: { period: string; value: number; } | { period: string; value: string; };
-        m3: { period: string; value: number; } | { period: string; value: string; };
-        m6: { period: string; value: number; } | {}; y1: {} | {}; y3: {} | {}; si: {} | {};
-        asOfDate: string;
-}[]
-
+    name: string;
+    m1: { period: string; value: number; } | { period: string; value: string; };
+    m3: { period: string; value: number; } | { period: string; value: string; };
+    m6: { period: string; value: number; } | {};
+    y1: {} | {};
+    y3: {} | {};
+    si: {} | {};
+    asOfDate: string;
+};
